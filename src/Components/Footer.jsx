3d08005e008f2ef2c2e17383839bc0c7583fad1d
Sujal Paliwal <sujalpaliwal214@gmail.com>
2026-02-09 18:27:00 +0530

    Change name for pwa app

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      <p className='text-[#d9d3d3] bg-[#01000097] w-4/5 py-1 text-center fixed bottom-0 left-[10%]' >Design and developed by Sujal Paliwal</p>
+      <p className='text-[#d9d3d3] bg-[#01000097] w-4/5 py-1 text-center fixed bottom-0 left-[10%]' >Design and Developed by Sujal Paliwal</p>
     </div>
   )
 }
